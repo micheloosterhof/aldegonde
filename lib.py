c4b0e1c37a129110305c87b231d8e288aa7978a8
Michel Oosterhof <michel@oosterhof.net>
2021-10-27 00:19:12 +0800

    add base conversion + brute force autokey

--- lib.py
+++ lib.py
@@ -96,6 +96,59 @@ def randomrunes(l: int, max: int = MAX) -> List[int]:
     return rl
 
 
+def numberToBase(n: int, b: int) -> List[int]:
+    """
+    converts from base10 to any other base. outputs as list of int
+    """
+    if n == 0:
+        return [0]
+    digits = []
+    while n:
+        digits.append(int(n % b))
+        n //= b
+    return digits[::-1]
+
+
+def base29(input: int, padding: int = -1) -> List[int]:
+    """
+    input `int` and output in Base29 as list of integers
+    """
+    l = numberToBase(input, MAX)
+    if padding == -1:
+        return l
+    else:
+        pad_value = 0
+        pad_size = padding - len(l)
+        final_list = [*[pad_value] * pad_size, *l]
+        return final_list
+
+
+class RuneIterator:
+    """
+    iterates over runes length L, [0,0,0], [0,0,1], [0,0,2], ..., [0,0,MAX], [0,1,0], ...
+    """
+
+    i: int
+    maximum: int
+    length: int
+
+    def __init__(self, length: int):
+        self.length = length
+        self.maximum = MAX ** length
+
+    def __iter__(self):
+        self.i = 0
+        return self
+
+    def __next__(self):
+        if self.i >= self.maximum:
+            raise StopIteration
+        else:
+            x = self.i
+            self.i += 1
+            return base29(x, padding=self.length)
+
+
 def diffstream(runes: List[int]) -> List[int]:
     """
     diffstream mod MAX
@@ -1023,12 +1076,12 @@ def bruteforce_autokey(
         "plaintext_minuend": plaintext_autokey_minuend_decrypt,
     }
     for keylength in range(minkeylength, maxkeylength + 1):
-        for key in range(0, (MAX ** keylength) + 1):
+        for key in RuneIterator(keylength):
             for a in algs.keys():
-                p = algs[a](ciphertext, [key])
+                p = algs[a](ciphertext, key)
                 ic = ioc(p)
                 if ic > iocthreshold:
-                    print(f"key {[key]}: {ic}: ")
+                    print(f"key {key}: {ic}: ")
                     english_output(p, limit=30)
 
     return
