38e26cb935314d8da7b64a683b6d0d71a656fb96
Michel Oosterhof <michel@oosterhof.net>
2022-05-28 17:48:35 +0800

    more test scenarios

--- aldegonde/algorithm/test_autokey.py
+++ aldegonde/algorithm/test_autokey.py
@@ -1,26 +1,33 @@
+#!/usr/bin/env python3
 
 from . import autokey
-from .. structures.alphabet import Alphabet, a2i
+from .. structures.alphabet import Alphabet, UPPERCASE_ALPHABET
 from .. structures.sequence import Sequence
 
-def test_autokey_vigenere():
-    # demo_alphabet = Alphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    demo_key = Sequence("X")
-    demo_plaintext = Sequence("NOTIFYQUARTERMASTER")
-    demo_ciphertext = a2i("KBHBNDOKURKXVDMSLXV")
+def test_plaintext_autokey_vigenere_x():
+    demo_key = Sequence("X", alphabet=UPPERCASE_ALPHABET)
+    demo_plaintext = Sequence("NOTIFYQUARTERMASTER", alphabet=UPPERCASE_ALPHABET)
+    demo_ciphertext = Sequence("KBHBNDOKURKXVDMSLXV", alphabet=UPPERCASE_ALPHABET)
     assert autokey.plaintext_autokey_vigenere_encrypt(plaintext=demo_plaintext, primer=demo_key) == demo_ciphertext
     assert autokey.plaintext_autokey_vigenere_decrypt(ciphertext=demo_ciphertext, primer=demo_key) == demo_plaintext
 
+def test_plaintext_autokey_vigenere_typewriter():
+    demo_key = Sequence("TYPEWRITER", alphabet=UPPERCASE_ALPHABET)
+    demo_plaintext = Sequence("NOTIFYQUARTERMASTER", alphabet=UPPERCASE_ALPHABET)
+    demo_ciphertext = Sequence("GMIMBPYNEIGSKUFQJYR", alphabet=UPPERCASE_ALPHABET)
+    assert autokey.plaintext_autokey_vigenere_encrypt(plaintext=demo_plaintext, primer=demo_key) == demo_ciphertext
+    assert autokey.plaintext_autokey_vigenere_decrypt(ciphertext=demo_ciphertext, primer=demo_key) == demo_plaintext
 
-def test_ciphertext_autokey_vigenere():
-    demo_key = Sequence("X")
-    demo_plaintext = Sequence("NOTIFYQUARTERMASTER")
-    demo_ciphertext = a2i("KYRZECSMMDWARDDVOSJ")
+def test_ciphertext_autokey_vigenere_x():
+    demo_key = Sequence("X", alphabet=UPPERCASE_ALPHABET)
+    demo_plaintext = Sequence("NOTIFYQUARTERMASTER", alphabet=UPPERCASE_ALPHABET)
+    demo_ciphertext = Sequence("KYRZECSMMDWARDDVOSJ", alphabet=UPPERCASE_ALPHABET)
     assert autokey.ciphertext_autokey_vigenere_encrypt(plaintext=demo_plaintext, primer=demo_key) == demo_ciphertext
     assert autokey.ciphertext_autokey_vigenere_encrypt(ciphertext=demo_ciphertext, primer=demo_key) == demo_plaintext
 
-
-
-
-
-
+def test_ciphertext_autokey_vigenere_typewriter():
+    demo_key = Sequence("TYPEWRITER", alphabet=UPPERCASE_ALPHABET)
+    demo_plaintext = Sequence("NOTIFYQUARTERMASTER", alphabet=UPPERCASE_ALPHABET)
+    demo_ciphertext = Sequence("GMIMBPYNEIGSKUFQJYR", alphabet=UPPERCASE_ALPHABET)
+    assert autokey.ciphertext_autokey_vigenere_encrypt(plaintext=demo_plaintext, primer=demo_key) == demo_ciphertext
+    assert autokey.ciphertext_autokey_vigenere_encrypt(ciphertext=demo_ciphertext, primer=demo_key) == demo_plaintext
