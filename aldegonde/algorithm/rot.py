3e3896717263ef6a16a2f9d381f0f559d30276d5
Michel Oosterhof <michel@oosterhof.net>
2022-06-01 16:17:19 +0800

    sequence ready

--- aldegonde/algorithm/rot.py
+++ aldegonde/algorithm/rot.py
@@ -1,9 +1,13 @@
-def rot(inp: list[int], shift: int) -> list[int]:
+from ..structures import sequence
+
+
+def rot(inp: sequence.Sequence, shift: int) -> sequence.Sequence:
     """
     shift contents of list by `shift` mod MAX
     e.g. rot13
     """
-    output = []
+    MAX = len(inp.alphabet)
+    output = sequence.Sequence(alphabet=inp.alphabet)
     for i in inp:
         output.append((i + shift + MAX) % MAX)
     return output
