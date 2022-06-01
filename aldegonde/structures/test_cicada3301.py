eee1ce4fbedf4a78e409a60d1b806c7c0d70cc15
Michel Oosterhof <michel@oosterhof.net>
2022-06-01 16:52:25 +0800

    more tests

--- aldegonde/structures/test_cicada3301.py
+++ aldegonde/structures/test_cicada3301.py
@@ -6,7 +6,10 @@ from .cicada3301 import *
 
 
 def test_welcome():
-    assert Sequence(text="ᚢᛠᛝᛋᛇᚠᚳᚱᛇᚢᚷᛈᛠᛠᚠᚹᛉᛏ", alphabet=CICADA_ALPHABET) == Sequence(
+    seq1 = Sequence(text="ᚢᛠᛝᛋᛇᚠᚳᚱᛇᚢᚷᛈᛠᛠᚠᚹᛉᛏ", alphabet=CICADA_ALPHABET)
+    assert seq1 == Sequence(
         data=[1, 28, 21, 15, 12, 0, 5, 4, 12, 1, 6, 13, 28, 28, 0, 7, 14, 16],
         alphabet=CICADA_ALPHABET,
     )
+    assert str(seq1) == "ᚢᛠᛝᛋᛇᚠᚳᚱᛇᚢᚷᛈᛠᛠᚠᚹᛉᛏ"
+    assert seq1[1] == 28
