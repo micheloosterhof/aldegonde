7c1da6bbea40bc6da4d95bd5d219e4c805d1961b
Michel Oosterhof <michel@oosterhof.net>
2022-06-02 10:22:55 +0800

    doublets

--- aldegonde/stats/doublets.py
+++ aldegonde/stats/doublets.py
@@ -5,6 +5,40 @@ from scipy.stats import poisson
 from ..structures import alphabet, sequence
 
 
+def print_doublets_statistics(runes: sequence.Sequence, skip: int = 1) -> list[int]:
+    """
+    find the number of doublets. doublet is X followed by X for any X
+    """
+    MAX = len(runes.alphabet)
+    N = len(runes)
+    doublets: list[int] = []
+    for index in range(0, N - skip):
+        if runes[index] == runes[index + skip]:
+            doublets.append(index)
+    l: int = len(doublets)
+    mu = N / MAX
+    mean, var = poisson.stats(mu, loc=0, moments="mv")
+    sigmage: float = abs(l - mean) / math.sqrt(var)
+    print(f"doublets={l} expected={mean:.2f} S={sigmage:.2f}σ")
+
+
+def doublets(runes: sequence.Sequence, skip: int = 1, trace: bool = False) -> list[int]:
+    """
+    find number of doublets. doublet is X followed by X for any X
+    """
+    MAX = len(runes.alphabet)
+    N = len(runes)
+    doublets: list[int] = []
+    for index in range(0, N - skip):
+        if runes[index] == runes[index + skip]:
+            doublets.append(index)
+            if trace:
+                print(
+                    f"doublet at {index}: {runes[index-1]}-{runes[index]}-{runes[index+1]}-{runes[index+2]}"
+                )
+    return doublets
+
+
 def triplets(runes: sequence.Sequence) -> int:
     """
     find number of triplet. triplet is X followed by XX for any X
@@ -15,30 +49,5 @@ def triplets(runes: sequence.Sequence) -> int:
     for index in range(0, N - 2):
         if runes[index] == runes[index + 1] and runes[index] == runes[index + 2]:
             triplets += 1
-    print(f"triplets = {triplets}")
-    expected = N / MAX / MAX
-    print(f"expected = {expected:.2f}")
+    # expected = N / MAX / MAX
     return triplets
-
-
-def doublets(runes: sequence.Sequence, skip: int = 1, trace: bool = False) -> list[int]:
-    """
-    find number of doublets. doublet is X followed by X for any X
-    """
-    MAX = len(runes.alphabet)
-    N = len(runes)
-    doublets: list[int] = []
-    for index in range(0, N - skip - 1):
-        if runes[index] == runes[index + skip]:
-            doublets.append(index)
-            if trace:
-                print(
-                    f"doublet at {index}: {runes[index-1]}-{runes[index]}-{runes[index+1]}-{runes[index+2]}"
-                )
-    l: int = len(doublets)
-
-    mu = N / MAX
-    mean, var = poisson.stats(mu, loc=0, moments="mv")
-    sigmage: float = abs(l - mean) / math.sqrt(var)
-    print(f"doublets={l} expected={mean:.2f} S={sigmage:.2f}σ")
-    return doublets
---
+++ aldegonde/stats/test_doublets.py
@@ -0,0 +1,20 @@
+from ..structures import sequence, alphabet
+from .doublets import doublets, triplets
+
+uniq = sequence.Sequence("ABCDEFGH", alphabet=alphabet.UPPERCASE_ALPHABET)
+dobl = sequence.Sequence("AABBCCDDEEFFGGHH", alphabet=alphabet.UPPERCASE_ALPHABET)
+trpl = sequence.Sequence(
+    "AAABBBCCCDDDEEEFFFGGGHHH", alphabet=alphabet.UPPERCASE_ALPHABET
+)
+
+
+def test_doublets():
+    assert doublets(dobl) == [0, 2, 4, 6, 8, 10, 12, 14]
+    assert doublets(uniq) == []
+    assert doublets(trpl) == [0, 1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16, 18, 19, 21, 22]
+
+
+def test_triplets():
+    assert triplets(dobl) == 0
+    assert triplets(uniq) == 0
+    assert triplets(trpl) == 8
