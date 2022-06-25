4ad4d688a15a7e2ee54031442c305775bca912a7
Michel Oosterhof <michel@oosterhof.net>
2022-06-25 11:58:29 +0800

    add tetragrams

--- aldegonde/grams/grams.py
+++ aldegonde/grams/grams.py
@@ -8,7 +8,7 @@ from typing import Dict
 from ..structures import sequence
 
 
-def trigrams(runes: sequence.Sequence, cut: int = 0) -> None:
+def trigrams(runes: sequence.Sequence, cut: int = 0) -> list[str]:
     """
     Specify `cut=0` and it operates on sliding blocks of 2 runes: ABC, BCD, CDE, ...
     Specify `cut=1` and it operates on non-overlapping blocks of 3 runes: ABC, DEF, ...
@@ -27,6 +27,34 @@ def trigrams(runes: sequence.Sequence, cut: int = 0) -> None:
         for i in range(cut - 1, N - 2, 3):
             try:
                 l.append(f"{runes[i]}-{runes[i+1]}-{runes[i+2]}")
+            except IndexError:
+                break
+    else:
+        raise Exception
+
+    return l
+
+
+def tetragrams(runes: sequence.Sequence, cut: int = 0) -> list[str]:
+    """
+    Specify `cut=0` and it operates on sliding blocks of 2 runes: ABCD, BCDE, CDEF, ...
+    Specify `cut=1` and it operates on non-overlapping blocks of 4 runes: ABCD, EFGH, ...
+    Specify `cut=2` and it operates on non-overlapping blocks of 4 runes: BCDE, FGHI, ...
+    Specify `cut=3` and it operates on non-overlapping blocks of 4 runes: CDEF, GHIJ, ...
+    Specify `cut=4` and it operates on non-overlapping blocks of 4 runes: DEFG, HIJK, ...
+    """
+    N = len(runes)
+    l: list = []
+    if cut == 0:
+        for i in range(0, N - 2):
+            try:
+                l.append(f"{runes[i]}-{runes[i+1]}-{runes[i+2]}-{runes[i+3]}")
+            except IndexError:
+                break
+    elif cut == 1 or cut == 2 or cut == 3 or cut == 4:
+        for i in range(cut - 1, N - 3, 3):
+            try:
+                l.append(f"{runes[i]}-{runes[i+1]}-{runes[i+2]}-{runes[i+3]}")
             except IndexError:
                 break
     else:
--- aldegonde/grams/test_grams.py
+++ aldegonde/grams/test_grams.py
@@ -3,7 +3,7 @@ test grams
 """
 
 from ..structures import alphabet, sequence
-from .grams import trigrams
+from .grams import trigrams, tetragrams
 
 nils = sequence.Sequence(data=[0] * 30, alphabet=alphabet.UPPERCASE_ALPHABET)
 ones = sequence.Sequence(data=[1] * 30, alphabet=alphabet.UPPERCASE_ALPHABET)
@@ -11,10 +11,19 @@ uniq = sequence.Sequence(data=list(range(0, 6)), alphabet=alphabet.UPPERCASE_ALP
 short = sequence.Sequence(data=[0] * 2, alphabet=alphabet.UPPERCASE_ALPHABET)
 
 
-def test_grams():
+def test_trigrams():
     """ """
     assert trigrams(short) == []
-    assert trigrams(uniq, cut=0) == ["0-1-2", "1-2-3","2-3-4", "3-4-5"]
+    assert trigrams(uniq, cut=0) == ["0-1-2", "1-2-3", "2-3-4", "3-4-5"]
     assert trigrams(uniq, cut=1) == ["0-1-2", "3-4-5"]
     assert trigrams(uniq, cut=2) == ["1-2-3"]
     assert trigrams(uniq, cut=3) == ["2-3-4"]
+
+
+def test_tetragrams():
+    """ """
+    assert tetragrams(short) == []
+    assert tetragrams(uniq, cut=0) == ["0-1-2-3", "1-2-3-4", "2-3-4-5"]
+    assert tetragrams(uniq, cut=1) == ["0-1-2-3"]
+    assert tetragrams(uniq, cut=2) == ["1-2-3-4"]
+    assert tetragrams(uniq, cut=3) == ["2-3-4-5"]
