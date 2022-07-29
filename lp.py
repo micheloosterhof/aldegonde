f5a46ad7eeec4309f72c62d264624203d0c2035c
Michel Oosterhof <michel@oosterhof.net>
2022-07-29 20:07:28 +0800

    update

--- lp.py
+++ lp.py
@@ -16,58 +16,62 @@ from aldegonde.analysis import kappa, isomorph
 from aldegonde.algorithm import autokey
 
 
-def try_totient(runes: list[int]):
-    """ """
-    from aldegonde.lib import phi_func
-
-    out = []
-    count = 1
-    for r in runes:
-        out.append((r - phi_func(count)) % 29)
-        count += 1
-    print(f"ENGLISH ioc={ioc.ioc(out):.3f} nioc={ioc.normalized_ioc(out):.3f}")
-    cicada3301.english_output(out, limit=30)
+def deltastream(runes: list[int], skip: int = 1) -> list[int]:
+    """
+    deltastream mod MAX
+    DIFF = C_K+1 - C_K % MAX
+    """
+    diff = []
+    for i in range(0, len(runes) - skip):
+        diff.append((runes[i + skip] - runes[i]) % 29)
+    return diff
 
 
 # with open("data/page54-55.txt") as f:
 with open("data/page0-58.txt") as f:
     lp = f.read()
 
-# segments = lp.split("&")
-segments = [lp]
+segments = lp.split("&")
+# segments = [lp]
 print(f"{len(segments)} segments")
 for i, s in enumerate(segments):
     if len(s) == 0:
         continue
     print(f"\n\nNEW SEGMENT {i} **************")
-
     seg = sequence.Sequence(s, alphabet=cicada3301.CICADA_ALPHABET)
-    if len(seg) == 0:
-        print("EMPTY SEGMENT {i}")
-        continue
-    # print(f"source: {s}")
-    print(f"full alphabet: {seg.alphabet}")
-    used = "".join([seg.alphabet[r] for r in set(seg.data)])
-    print(f"used alphabet: {used} {len(set(seg.data))} items")
-    # print(f"ciphertext: {seg.elements}")
-    print(f"length: {len(seg)} runes")
-    cicada3301.print_all(seg, limit=30)
-    dist.print_dist(seg)
-    print(f"ioc={ioc.ioc(seg):.3f} nioc={ioc.normalized_ioc(seg):.3f}")
-    bigram_diagram.print_bigram_diagram(seg)
-    bigram_diagram.bigram_diagram_skip(seg, skip=2)
-    doublets.print_doublets_statistics(seg)
-    doublets.print_doublets_statistics(seg, skip=2)
-    repeats.print_repeat_statistics(seg, minimum=2)
-    kappa.print_kappa(seg)
-    reps = repeats.repeat2(seg, minimum=5)
-    diffs = []
-    for key in reps.keys():
-        positions = reps[key]
-        for v in itertools.combinations(positions, 2):
-            print(
-                f"repeat: {key} loc={v[1]},{v[0]} diff={abs(v[1]-v[0])} factors={factor.prime_factors(abs(v[1]-v[0]))}"
-            )
+    for skip in range(1, 2):
+        # print(f"skip={skip}")
+        # seg = sequence.Sequence(data=deltastream(seg1.data, skip=skip), alphabet=cicada3301.CICADA_ALPHABET)
+        if len(seg) == 0:
+            print("EMPTY SEGMENT {i}")
+            continue
+        # print(f"source: {s}")
+        print(f"full alphabet: {seg.alphabet}")
+        used = "".join([seg.alphabet[r] for r in set(seg.data)])
+        print(f"used alphabet: {used} {len(set(seg.data))} items")
+        # print(f"ciphertext: {seg.elements}")
+        print(f"length: {len(seg)} runes")
+        cicada3301.print_all(seg, limit=30)
+        dist.print_dist(seg)
+        ioc.print_ioc_statistics(seg)
+        bigram_diagram.print_bigram_diagram(seg)
+        bigram_diagram.bigram_diagram_skip(seg, skip=2)
+        doublets.print_doublets_statistics(seg)
+        doublets.print_doublets_statistics(seg, skip=2)
+        repeats.print_repeat_statistics(seg, minimum=2)
+        kappa.print_kappa(seg)
+        reps = repeats.repeat2(seg, minimum=5)
+        diffs = []
+        for key in reps.keys():
+            positions = reps[key]
+            for v in itertools.combinations(positions, 2):
+                print(
+                    f"repeat: {key} loc={v[1]},{v[0]} diff={abs(v[1]-v[0])} factors={factor.prime_factors(abs(v[1]-v[0]))}"
+                )
 
-    print("\n")
-    isomorph.print_isomorph_statistics(seg)
+        print("\n")
+        # isomorph.print_isomorph_statistics(seg)
+
+        slide = ioc.sliding_window_ioc(seg, window=100)
+        for i, e in enumerate(slide):
+            print(f"{i:5d} {e:.3f}")
