b59bd486960efc02a9bd255a097b558a52a5ac32
Michel Oosterhof <michel@oosterhof.net>
2022-07-30 22:28:05 +0800

    udpates

---
+++ aldegonde/math/totient.py
@@ -0,0 +1,20 @@
+def gcd(p, q):
+    # Create the gcd of two positive integers.
+    while q != 0:
+        p, q = q, p % q
+    return p
+
+
+def is_coprime(x, y):
+    return gcd(x, y) == 1
+
+
+def phi_func(x):
+    """
+    euler phi // totient
+    """
+    if x == 1:
+        return 1
+    else:
+        n = [y for y in range(1, x) if is_coprime(x, y)]
+        return len(n)
