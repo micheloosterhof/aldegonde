644540df6a9723770714c50ae839ac33a7f49eff
Michel Oosterhof <michel@oosterhof.net>
2022-08-14 11:17:54 +0800

    remove comment

--- aldegonde/stats/gtest.py
+++ aldegonde/stats/gtest.py
@@ -1,6 +1,6 @@
 """
 https://en.wikipedia.org/wiki/G-test
-use scipy.stats.power_divergence with lambda_=0.[12]
+use scipy.stats.power_divergence with lambda_=0
 """
 
 import scipy.stats.power_divergence
