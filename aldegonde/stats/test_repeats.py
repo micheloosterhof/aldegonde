321b8a4054c0167254e585bbc735d537f9cf6124
Michel Oosterhof <michel@oosterhof.net>
2022-09-30 14:18:53 +0800

    repeat test

---
+++ aldegonde/stats/test_repeats.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from ..structures import alphabet, sequence
+
+from .repeats import repeat, repeat2
+
+uniq = sequence.Sequence.fromstr(
+    text="ABCABC", alphabet=alphabet.UPPERCASE_ALPHABET
+)
+
+def test_ngram_unique() -> None:
+    assert(repeat(uniq) == {'0-1': 2, '1-2': 2, '0-1-2': 2})
+    assert(repeat2(uniq) == {'0-1': [0, 3], '1-2': [1, 4], '0-1-2': [0, 3]})
