738df5a7a1f9dda98e5785441adbf7c4e557c6c8
Michel Oosterhof <michel@oosterhof.net>
2022-11-03 20:51:24 +0800

    a bit cleaner

--- aldegonde/analysis/isomorph.py
+++ aldegonde/analysis/isomorph.py
@@ -7,6 +7,7 @@
     length 4: AAAA | AAAB AABA AABB ABAA ABAB ABBA ABBB |
               AABC AACB ABAC ABBC ABCA ABCB ABCC ABCA ABCB ABCC | ABCD
 """
+from collections import defaultdict
 import itertools
 import random
 import statistics
@@ -38,18 +39,23 @@ def all_isomorphs(ciphertext: sequence.Sequence, length: int) -> dict[str, list[
     """
     Return all isomorphs of a particular length from a sequence
     """
-    isos: dict[
-        str, list[int]
-    ] = {}  # normalized isomorph as key, list of positions as value
-    isos = {}
-
+    isos: dict[str, list[int]] = defaultdict(
+        list
+    )  # normalized isomorph as key, list of positions as value
     for pos in range(0, len(ciphertext) - length):
         iso = isomorph(ciphertext[pos : pos + length])
-        if iso in isos:
-            isos[iso].append(pos)
-        else:
-            isos[iso] = [pos]
+        isos[iso].append(pos)
     return isos
+
+
+def isomorph_statistics(isomorphs: dict[str, list[int]]) -> (int, int):
+    """
+    Input is the output of `all_isomorphs`.
+    Returns `distincts` and `duplicates` for the input
+    """
+    distinct: int = len(isomorphs.keys())
+    duplicate: int = sum([len(x) for x in isomorphs.values() if len(x) > 1])
+    return (distinct, duplicate)
 
 
 def random_isomorph_statistics(
@@ -58,27 +64,20 @@ def random_isomorph_statistics(
     """
     Returns the mean and stdev of distinct isomorphs and mean and stdev of duplicate isomorphs
     """
-    distincts: Dict[int, list[int]] = {}
-    duplicates: Dict[int, list[int]] = {}
+    distincts: list[int] = []
+    duplicates: list[int] = []
 
     for _ in range(0, samples):
-        # create random samples
         rand: list[int] = [random.randrange(0, 29) for _ in range(sequencelength)]
         isos = all_isomorphs(rand, isomorphlength)
-        distinct: int = len(isos.keys())
-        duplicate: int = sum([len(x) for x in isos.values() if len(x) > 1])
+        (distinct, duplicate) = isomorph_statistics(isos)
+        distincts.append(distinct)
+        duplicates.append(duplicate)
 
-        if isomorphlength in distincts:
-            distincts[isomorphlength].append(distinct)
-            duplicates[isomorphlength].append(duplicate)
-        else:
-            distincts[isomorphlength] = [distinct]
-            duplicates[isomorphlength] = [duplicate]
-
-    meandistinct = statistics.mean(distincts[isomorphlength])
-    stdevdistinct = statistics.stdev(distincts[isomorphlength])
-    meanduplicate = statistics.mean(duplicates[isomorphlength])
-    stdevduplicate = statistics.stdev(duplicates[isomorphlength])
+    meandistinct = statistics.mean(distincts)
+    stdevdistinct = statistics.stdev(distincts)
+    meanduplicate = statistics.mean(duplicates)
+    stdevduplicate = statistics.stdev(duplicates)
     return (meandistinct, stdevdistinct, meanduplicate, stdevduplicate)
 
 
@@ -95,11 +94,8 @@ def print_isomorph_statistics(seq: sequence.Sequence, trace: bool = False) -> No
 
     for length in range(startlength, endlength + 1):
         isos = all_isomorphs(seq, length)
-        # print(isos.keys())
-        distinct: int = len(isos.keys())
-        duplicates: int = sum([len(x) for x in isos.values() if len(x) > 1])
-
-        if duplicates < 100 or trace is True:
+        (distinct, duplicate) = isomorph_statistics(isos)
+        if duplicate < 100 or trace is True:
             for key, values in isos.items():
                 for v in itertools.combinations(values, 2):
                     print(
@@ -113,24 +109,24 @@ def print_isomorph_statistics(seq: sequence.Sequence, trace: bool = False) -> No
             stdevduplicate,
         ) = random_isomorph_statistics(sequencelength=len(seq), isomorphlength=length)
 
-        if duplicates == 0:
+        if duplicate == 0:
             print(f"no duplicate isomorphs found of length {length}")
             break
 
         print(f"isomorphs length {length:2d}: distinct isomorphs:", end=" ")
         try:
             print(
-                f"{distinct:3d} (avg: {avgdistinct:6.2f}, S={abs(avgdistinct-distinct)/stdevdistinct:4.2f}σ)",
+                f"{distinct:5d} (avg: {avgdistinct:8.2f}, S={abs(avgdistinct-distinct)/stdevdistinct:5.2f}σ)",
                 end=" ",
             )
         except ZeroDivisionError:
             print(
-                f"{distinct:3d} (avg: {avgdistinct:6.2f}",
+                f"{distinct:5d} (avg: {avgdistinct:8.2f}",
                 end=" ",
             )
         try:
             print(
-                f"duplicate: {duplicates:4d} (avg: {avgduplicate:7.2f} S={abs(avgduplicate-duplicates)/stdevduplicate:4.2f}σ) "
+                f"duplicate: {duplicate:5d} (avg: {avgduplicate:8.2f} S={abs(avgduplicate-duplicate)/stdevduplicate:5.2f}σ) "
             )
         except ZeroDivisionError:
-            print(f"duplicate: {duplicates:4d} (avg: {avgduplicate:7.2f})")
+            print(f"duplicate: {duplicate:5d} (avg: {avgduplicate:8.2f})")
