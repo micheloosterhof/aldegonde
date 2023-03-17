023931cf7d4dac8aa2cb18761bd8a6b619bf0554
Michel Oosterhof <michel@oosterhof.net>
2023-03-17 12:59:24 +0800

    add exptable

---
+++ examples/exptable.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def exptable(size: int = 29):
+    for row in range(0, size + 1):
+        print(f"{row:02d} | ", end="")
+        for col in range(0, size + 1):
+            exp = pow(col,row, size)
+            print(f"{exp:02d} ", end="")
+        print()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <size>")
+        sys.exit(1)
+    exptable(int(sys.argv[1]))
