2e11992be9e39af3d5569cb9356b71cf352d9c5e
Michel Oosterhof <michel@oosterhof.net>
2023-03-17 16:34:07 +0800

    add isomorph test

--- src/aldegonde/stats/test_isomorph.py
+++ src/aldegonde/stats/test_isomorph.py
@@ -1,7 +1,11 @@
 """tests for doublets.py"""
 
-from aldegonde.stats.isomorph import isomorph
+from aldegonde.stats.isomorph import isomorph, all_isomorphs
 
 
-def test_isomorphs():
+def test_isomorph():
     assert isomorph("ATTACK") == isomorph("EFFECT")
+
+
+def test_all_isomorphs():
+    assert all_isomorphs("ATTACK", length=4) == {"ABBA": [0], "AABC": [1]}
