db634f3946759374283289ad958c4d2bd3f5e537
Michel Oosterhof <michel@oosterhof.net>
2023-03-18 12:00:53 +0800

    simplified

--- src/aldegonde/stats/dist.py
+++ src/aldegonde/stats/dist.py
@@ -1,34 +1,34 @@
 from collections import Counter
+from collections.abc import Sequence
+from typing import TypeVar
 
-from aldegonde.structures import sequence
 from aldegonde.stats.ngrams import iterngrams
 
+T = TypeVar("T")
 
-def print_dist(runes: sequence.Sequence) -> None:
+
+def print_dist(runes: Sequence[T]) -> None:
     """
     print frequency distribution
     """
     N = len(runes)
     col = 0
-    freqs = Counter(runes)
+    freqs = dist(runes)
+    print(freqs)
     print("frequency distribution:")
-    for i, e in enumerate(runes.alphabet):
+    for i, e in enumerate(freqs.keys()):
         if col > 0 and col % 5 == 0:
             print("")
         print(
-            f"{i:02d}: {e}: {freqs[i]:03d}: {freqs[i]/N*100:.3f}% | ",
+            f"#{i:02d}: {e:4s}: {freqs[e]:03d}: {freqs[e]/N*100:.3f}% | ",
             end="",
         )
         col = col + 1
     print("")
 
 
-def dist(runes: sequence.Sequence, length: int = 1, cut: int = 0) -> dict[str, int]:
+def dist(text: Sequence[T], length: int = 1, cut: int = 0) -> dict[str, int]:
     """
-    flexible dist function
+    flexible dist function, returns ngrams by count
     """
-    out: dict[str, int] = {}
-    for g in iterngrams(runes, length=length, cut=cut):
-        k = "-".join([str(x) for x in g])
-        out[k] = out.get(k, 0) + 1
-    return out
+    return Counter([str(g) for g in iterngrams(text, length=length, cut=cut)])
