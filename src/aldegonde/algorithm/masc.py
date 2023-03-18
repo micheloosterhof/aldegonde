03b1bd30a870d45d09d9bdc328634edc8f13d2cc
Michel Oosterhof <michel@oosterhof.net>
2023-03-18 21:01:28 +0800

    updated MASC

--- src/aldegonde/algorithm/masc.py
+++ src/aldegonde/algorithm/masc.py
@@ -1,60 +1,80 @@
 """monoalphabetic substitution cipher
+
+This contains all ciphers of this type. This includes Caesar, ROT13, Affine ciphers, etc
+All are monoalphabetic 
 """
 
+
+from collections.abc import Sequence
 import random
+from typing import TypeVar
 
-from aldegonde.structures.alphabet import Alphabet
-from aldegonde.structures.sequence import Sequence
+T = TypeVar("T")
 
 
-def monoalphabetic_substitution_encrypt(
-    plaintext: Sequence, key: list[int]
-) -> Sequence:
+def masc_encrypt(plaintext: Sequence[T], key: dict[T, T]) -> list[T]:
     """
     Monalphabetic substitution
     """
-    output = []
+    output: list = []
     for e in plaintext:
         output.append(key[e])
-    outputseq: Sequence = Sequence.fromlist(
-        data=output, alphabet=plaintext.alphabet.alphabet
-    )
-    return outputseq
-
-
-def reverse_key(key: list[int]) -> list[int]:
-    """Takes an array containing all elements and reverses the index and the value
-    Returns output if the input contains valid values, else raises ValueError
-    """
-    output = [-1] * len(key)
-    for i in key:
-        try:
-            output[key[i]] = i
-        except IndexError:
-            raise ValueError
-    for i in output:
-        if i == -1:
-            raise ValueError
     return output
 
 
-def monoalphabetic_substitution_decrypt(
-    ciphertext: Sequence, key: list[int]
-) -> Sequence:
+def reverse_key(key: dict[T, T]) -> dict[T, T]:
+    """
+    Takes a dict containing all elements and reverses the index and the value
+    Returns output if the input contains valid values, else raises ValueError
+    """
+    output: dict[T, T] = {}
+    for k, v in key.items():
+        output[v] = k
+    return output
+
+
+def masc_decrypt(ciphertext: Sequence[T], key: dict[T, T]) -> list[T]:
     """Monalphabetic substitution
     NOTE: key input is the same as for encryption, this function will reverse the key
     """
-    reversed_key = reverse_key(key)
-    print(f"revkey: {reversed_key}")
-    output = []
+    reversed_key: dict[T, T] = reverse_key(key)
+    output: list = []
     for e in ciphertext:
         output.append(reversed_key[e])
-    outputseq: Sequence = Sequence.fromlist(
-        data=output, alphabet=ciphertext.alphabet.alphabet
-    )
-    return outputseq
+    return output
 
 
-def randomkey(length: int) -> list[int]:
-    """Generate a random key for use the previous functions"""
-    return random.sample(list(range(length)), length)
+def randomkey(alphabet: Sequence[T]) -> dict[T, T]:
+    """Generate a random key for use in the previous functions"""
+    key: dict[T, T] = {}
+    shuffled = random.sample(alphabet, len(alphabet))
+    for k, v in zip(alphabet, shuffled):
+        key[k] = v
+    return key
+
+
+def shiftedkey(alphabet: Sequence[T], shift: int = 3) -> dict[T, T]:
+    """
+    Generate a shifted key for use in the previous functions
+    use `3` for Caesar and `13` for ROT13
+    """
+    key: dict[T, T] = {}
+    for i, e in enumerate(alphabet):
+        key[e] = alphabet[(i + shift) % len(alphabet)]
+    return key
+
+
+def affinekey(alphabet: Sequence[T], a: int = 3, b: int = 8) -> dict[T, T]:
+    """
+    Generate an affine key for use in the previous functions
+    Parameter A must be coprime to the alphabet length to generate a valid encoding
+    """
+    key: dict[T, T] = {}
+    for i, e in enumerate(alphabet):
+        key[e] = alphabet[(a * i + b) % len(alphabet)]
+    if set(key.keys()) != set(key.values()):
+        print(f"{key.items()} {key.values()}")
+        raise ValueError(
+            "Invalid Affine cipher parameter: A={a} is not coprime with the size of the alphabet {len(alphabet)}"
+        )
+    return key
--- src/aldegonde/algorithm/test_masc.py
+++ src/aldegonde/algorithm/test_masc.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
 
-from aldegonde.structures.alphabet import UPPERCASE_ALPHABET
-from aldegonde.structures.sequence import Sequence
-from aldegonde.algorithm import masc
+from aldegonde import masc
+
+ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ALICE = (
+    "ALICEWASBEGINNINGTOGETVERYTIREDOFSITTINGBYHERSISTERONTHEBANKANDOFHAVINGNOTHINGTO"
+)
 
 
-def test_masc() -> None:
-    plaintext = Sequence.fromstr(text="ATTACKATDAWN", alphabet=UPPERCASE_ALPHABET)
-    key = masc.randomkey(length=len(plaintext.alphabet))
-    print(key)
-    ciphertext = masc.monoalphabetic_substitution_encrypt(plaintext, key)
-    print(ciphertext)
-    assert plaintext == masc.monoalphabetic_substitution_decrypt(
-        ciphertext=ciphertext, key=key
+def test_masc_encrypt_random() -> None:
+    rnd = masc.randomkey(ABC)
+    ciphertext = masc.masc_encrypt(ALICE, key=rnd)
+    plaintext = masc.masc_decrypt(ciphertext, key=rnd)
+    assert list(ALICE) == plaintext
+
+
+def test_masc_encrypt_caesar() -> None:
+    caesar = masc.shiftedkey(ABC, shift=3)
+    ciphertext = masc.masc_encrypt(ALICE, key=caesar)
+    plaintext = masc.masc_decrypt(ciphertext, key=caesar)
+    assert list(ALICE) == plaintext
+
+
+def test_reverse_key() -> None:
+    assert masc.shiftedkey(ABC, shift=13) == masc.reverse_key(
+        masc.shiftedkey(ABC, shift=13)
     )
+
+
+def test_shiftedkey() -> None:
+    assert masc.shiftedkey(ABC, shift=3) == masc.shiftedkey(ABC, shift=-23)
+    assert masc.shiftedkey(ABC, shift=3) == masc.shiftedkey(ABC, shift=29)
+
+
+def test_affinekey() -> None:
+    assert masc.affinekey(ABC, a=3, b=8) == masc.affinekey(ABC, a=3, b=8)
+    try:
+        assert masc.affinekey(ABC, a=2, b=8) == masc.affinekey(ABC, a=2, b=8)
+    except ValueError:
+        pass
