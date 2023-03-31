de565171973f6e61259910c0b92953d69ba75696
Michel Oosterhof <michel@oosterhof.net>
2023-03-31 10:29:10 +0800

    cohort 1st exercises

---
+++ cohort/1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Cohort 1st challenge
+# 1. Detect Monoalphabetic Substitution Cipher
+# 2. Assume shift cipher/atbash or combo and give most likely answer
+
+# Implementation
+# 1. Score with IOC to detect MASC, check if in range of normal language
+# 2. Test shift/atbash combos and score with chisquare test
+# 3. Print results
