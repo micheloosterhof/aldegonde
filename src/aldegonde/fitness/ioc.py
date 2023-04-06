fc5a8f24c099c012a8bc3c05366c15a20f7bc5ae
Michel Oosterhof <michel@oosterhof.net>
2023-04-06 23:12:11 +0800

    remove sequence

--- src/aldegonde/fitness/ioc.py
+++ src/aldegonde/fitness/ioc.py
@@ -3,10 +3,12 @@ IOC Scoring Function
 """
 
 from math import sqrt
-from collections.abc import Callable
+from collections.abc import Callable, Sequence
+from typing import TypeVar
 
-from aldegonde.structures import sequence
 from aldegonde.stats import ioc
+
+T = TypeVar("T")
 
 ENGLISH = 0.0667
 FRENCH = 0.0778
@@ -16,7 +18,7 @@ ITALIAN = 0.0738
 RUSSIAN = 0.0529
 
 
-def IocFitness(target_ioc: float) -> Callable[[sequence.Sequence], float]:
+def IocFitness(target_ioc: float) -> Callable[[Sequence[T]], float]:
     """Score a text by evaluating its IOC score
 
     Example:
@@ -28,7 +30,7 @@ def IocFitness(target_ioc: float) -> Callable[[sequence.Sequence], float]:
         target_ioc (lfoat): symbol to frequency mapping of the distribution to compare with
     """
 
-    def inner(text: sequence.Sequence) -> float:
+    def inner(text: Sequence[T]) -> float:
         return -sqrt(abs(ioc.ioc(text) - target_ioc))
 
     return inner
