3df08283fd930dc4091e230910f9bd4646bbbd62
Michel Oosterhof <michel@oosterhof.net>
2023-04-13 10:19:20 +0800

    remove sequence

--- src/aldegonde/analysis/split.py
+++ src/aldegonde/analysis/split.py
@@ -1,45 +1,46 @@
 """Functions to split a text in various ways"""
 
-from aldegonde.structures import sequence
+from collections.abc import Sequence
+from typing import TypeVar
+
+T = TypeVar("T")
 
 
-def split_by_slice(inp: sequence.Sequence, size: int) -> dict[int, sequence.Sequence]:
+def split_by_slice(inp: Sequence[T], size: int) -> dict[int, Sequence[T]]:
     """
     Create slices of the input, with a certain slice size
     This will return every N'th element
     For size 3, it will return a dictionary of lists with elements:
        [0, 3, 6, 9] [1, 4, 7, 10] [2, 5, 8]
     """
-    outp = {}
+    outp: dict[int, Sequence[T]] = {}
     for i in range(0, size):
-        ra = inp[slice(i, len(inp), size)].copy()
-        outp[i] = sequence.Sequence.fromlist(data=ra, alphabet=inp.alphabet.alphabet)
+        outp[i] = inp[slice(i, len(inp), size)]
     return outp
 
 
-def split_by_character(inp: sequence.Sequence) -> dict[int, sequence.Sequence]:
+def split_by_character(inp: Sequence[T]) -> dict[T, list[T]]:
     """
     by previous character
     """
-    MAX = len(inp.alphabet)
-    outp: dict[int, sequence.Sequence] = {}
-    for i in range(0, MAX):
-        outp[i] = sequence.Sequence(alphabet=inp.alphabet)
+    outp: dict[T, list[T]] = {}
+    for e in set(inp):
+        outp[e] = []
     for i in range(0, len(inp) - 1):
         outp[inp[i]].append(inp[i + 1])
     return outp
 
 
-def split_by_doublet(ciphertext: sequence.Sequence) -> list[sequence.Sequence]:
+def split_by_doublet(ciphertext: Sequence[T]) -> list[list[T]]:
     """
     split in simple chunks separated by a doublet
     """
-    output: list[sequence.Sequence] = []
-    current = sequence.Sequence(alphabet=ciphertext.alphabet)
+    output: list[list[T]] = []
+    current: list[T] = []
     for i in range(0, len(ciphertext)):
         if ciphertext[i] == ciphertext[i - 1]:
             output.append(current)
-            current = sequence.Sequence(data=[i], alphabet=ciphertext.alphabet)
+            current = [ciphertext[i]]
         else:
             current.append(ciphertext[i])
     output.append(current)
