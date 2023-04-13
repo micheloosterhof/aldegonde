d81946633235ea3fd2710192b135a0cdf3c6eaaa
Michel Oosterhof <michel@oosterhof.net>
2023-04-13 17:37:12 +0800

    black

--- src/aldegonde/structures/test_cicada3301.py
+++ src/aldegonde/structures/test_cicada3301.py
@@ -4,6 +4,6 @@ from aldegonde.structures.cicada3301 import CICADA_ALPHABET
 
 
 def test_welcome() -> None:
-    data=[1, 28, 21, 15, 12, 0, 5, 4, 12, 1, 6, 13, 28, 28, 0, 7, 14, 16]
+    data = [1, 28, 21, 15, 12, 0, 5, 4, 12, 1, 6, 13, 28, 28, 0, 7, 14, 16]
     seq = "".join([CICADA_ALPHABET[e] for e in data])
     assert seq == "ᚢᛠᛝᛋᛇᚠᚳᚱᛇᚢᚷᛈᛠᛠᚠᚹᛉᛏ"
