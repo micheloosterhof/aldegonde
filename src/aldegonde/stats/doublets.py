9bfbcf1937077a4b7d0dce5f33aa1ece5e5974c9
Michel Oosterhof <michel@oosterhof.net>
2023-04-14 14:01:15 +0800

    update

--- src/aldegonde/stats/doublets.py
+++ src/aldegonde/stats/doublets.py
@@ -17,10 +17,9 @@ def print_doublets_statistics(
     """
     find the number of doublets. doublet is X followed by X for any X
     """
-    N: int = len(runes)
     dbls: list[int] = doublets(runes, skip=skip)
     l: int = len(dbls)
-    mu = N / alphabetsize
+    mu = len(runes) / alphabetsize
     mean, var = poisson.stats(mu, loc=0, moments="mv")
     sigmage: float = abs(l - mean) / sqrt(var)
     print(f"doublets={l} (skip={skip}) expected={mean:.2f} S={sigmage:.2f}σ")
@@ -30,16 +29,15 @@ def doublets(runes: Sequence[T], skip: int = 1, trace: bool = False) -> list[int
     """
     find number of doublets. doublet is X followed by X for any X
     """
-    N = len(runes)
-    dbls: list[int] = []
-    for index in range(0, N - skip):
+    positions: list[int] = []
+    for index in range(0, len(runes) - skip):
         if runes[index] == runes[index + skip]:
-            dbls.append(index)
+            positions.append(index)
             if trace:
                 print(
                     f"doublet at {index}: {runes[index-1]}-{runes[index]}-{runes[index+1]}-{runes[index+2]}"
                 )
-    return dbls
+    return positions
 
 
 def triplets(runes: Sequence[T]) -> int:
