25ebf8a5121cf23420e8969e35e88e37f7d60ddf
Michel Oosterhof <michel@oosterhof.net>
2023-05-04 17:22:06 +0800

    add pgsc

--- src/aldegonde/algorithm/pgsc.py
+++ src/aldegonde/algorithm/pgsc.py
@@ -21,14 +21,36 @@ function = Callable[[Sequence[T]], Sequence[T]]
 
 
 def pgsc_encrypt(
-    plaintext: Sequence[T], length: int, encryptfn: function
+    plaintext: Sequence[T], length: int, encryptfn: function, padding: Sequence[T]
 ) -> tuple[T, ...]:
     """Polygraphic substitution."""
-    return tuple(plaintext)
+    ciphertext: list[T] = []
+    # TODO: padding
+    for i in range(0, len(plaintext), length):
+        ciphertext.extend(encryptfn(plaintext[i : i + length]))
+    return tuple(ciphertext)
 
 
 def pgsc_decrypt(
-    ciphertext: Sequence[T], length: int, decryptfn: function
+    ciphertext: Sequence[T], length: int, decryptfn: function, padding: Sequence[T]
 ) -> tuple[T, ...]:
     """Polygraphic substitution."""
     return tuple(ciphertext)
+    plaintext: list[T] = []
+    for i in range(0, len(plaintext), length):
+        plaintext.extend(decryptfn(plaintext[i : i + length]))
+    return tuple(plaintext)
+
+
+def playfair_encrypt(plaintext: Sequence[T], keyword: Sequence[T]) -> tuple[T, ...]:
+    """playfair encrypt"""
+    square = playfair_square(keyword)
+    encryptfn = playfair_bigram_encrypt(square)
+    return pgsc_encrypt(plaintext, length=2, encryptfn=encryptfn)
+
+
+def playfair_decrypt(ciphertext: Sequence[T], keyword: Sequence[T]) -> tuple[T, ...]:
+    """playfair decrypt"""
+    square = playfair_square(keyword)
+    encryptfn = playfair_bigram_decrypt(square)
+    return pgsc_decrypt(ciphertext, length=2, decryptfn=decryptfn)
