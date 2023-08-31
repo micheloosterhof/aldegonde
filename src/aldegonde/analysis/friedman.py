aedb05626a879944a436cccb4c1be426d44f9490
Michel Oosterhof <michel@oosterhof.net>
2023-08-31 11:05:34 +0800

    also calculate median ioc

--- src/aldegonde/analysis/friedman.py
+++ src/aldegonde/analysis/friedman.py
@@ -1,6 +1,7 @@
 """Friedman test to detect use of the same alphabet at regular intervals."""
 
 from collections.abc import Sequence
+from statistics import mean, median
 from typing import TypeVar
 
 from aldegonde.stats.ioc import ioc
@@ -22,26 +23,28 @@ def friedman_test(
     # avgioc contains the key length as key. as value it's the avgioc
     # delta is the difference between the avgioc and the max of avgioc of all lower values
     avgioc: dict[int, float] = {}
+    medioc: dict[int, float] = {}
     deltas: dict[int, float] = {}
 
     if trace is True:
         print("Testing for periodicity using friedman test")
 
-    print("Candidates:")
     for period in range(minperiod, maxperiod + 1):
-        iocsum: float = 0.0
+        iocs: list[float] = []
         for k in range(0, period):
             v = ciphertext[slice(k, len(ciphertext), period)]
             ic: float = ioc(v)  # note, perviously was normalized
-            iocsum += ic
+            iocs.append(ic)
             if trace is True:
                 print(f"ioc of slice {k}/{period} = {ic:.3f}")
-        avgioc[period] = iocsum / period
+        medioc[period] = median(iocs)
+        avgioc[period] = mean(iocs)
         deltas[period] = avgioc[period] - max(avgioc.values())
         print(
-            f"friedman: period {period:02d} avgioc: {avgioc[period]:.3f} delta: {deltas[period]:+.4f}",
+            f"friedman: period {period:02d} medioc: {medioc[period]:.3f} avgioc: {avgioc[period]:.3f} delta: {deltas[period]:+.4f}",
             end="",
         )
+
         if abs(deltas[period]) < 0.001:
             print(" <===")
         else:
