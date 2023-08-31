49cbfa4b7f93f58f4a8e10a43e1d70c9d8702c86
Michel Oosterhof <michel@oosterhof.net>
2023-08-31 13:10:32 +0800

    remove typing warnign

--- src/aldegonde/stats/compare.py
+++ src/aldegonde/stats/compare.py
@@ -132,7 +132,7 @@ def NgramScorer(frequency_map: dict[str, int]) -> Callable[[str], float]:
     )
 
     def inner(text: str) -> float:
-        gen = (ngrams[ngram] for ngram in iterngrams(text, length))
+        gen = (ngrams[str(ngram)] for ngram in iterngrams(text, length))
         return sum(gen)
 
     return inner
