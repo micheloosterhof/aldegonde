861caebcd94c685f6b6b3957a21d66f3bb52ba6d
Michel Oosterhof <michel@oosterhof.net>
2023-12-22 12:55:26 +0800

    add crack shift

---
+++ examples/crack_shift.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# IDEAS: simmulated annealing
+# IDEAS: steepest ascent (is practical? 28x27=756 evaluations)
+
+from collections.abc import Callable, Sequence
+import random
+import copy
+from typing import NamedTuple, TypeVar
+
+from aldegonde import masc
+from aldegonde.stats import compare, ngrams
+
+T = TypeVar("T")
+
+CAE="WTLPHDCRTPAXIIATVGTTCQPAADURAPNVJBQNQJINDJHWDJASHTTLWPIWTRPCSDIDSPNVJBQNWTRPCLPAZXCIDBPCNQDDZHLXIWWXHEDCTNEPAEDZTNIDDPCSXUNDJWPKTPWTPGIIWTCVJBQNHPEPGIDUNDJVJBQN"
+
+DISCO="MFFTUEFUYQOMQEMDIMEQXQOFQPRADFTQRAGDFTFUYQMEPUOFMFADMZPIMEOAYUZSFAEBMUZFARUZUETFTQIMDAZFTQIMKTQIMEYQFNKMYNMEEMPADERDAYOADPANMITATMPPQEQDFQPSQZQDMXBAYBQKFTQKUZRADYQPTUYFTMFUFIAGXPNQQMEUQEFFAFMWQFTQOUFKMFZUSTFNQOMGEQFTQQZQYKTMPNKFTQZZAWZAIXQPSQARPUEOAADARFTQZUSTFXURQADARFTQNAASUQ"
+
+WLA="WLALYWPWLYWPJRLKHWLJRVMWPJRSLKWLWWLYZ"
+KLY="KLYJKPMCLDUFXAESCZFRSSZZADLEESPKZZ"
+
+
+
+CT = WLA.replace(" ", "").replace(",", "").replace(".", "").replace("\n", "")
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def brute():
+    """
+    bruteforce all shifts
+    """
+    out: list[tuple[int, str, float]] = []
+    for shift in range(len(alphabet)):
+        key = masc.shiftedkey(alphabet, shift)
+        pt = "".join(masc.masc_decrypt(CT, key))
+        score = compare.quadgramscore(pt)
+        out.append(tuple([shift,pt,score]))
+    for p in sorted(out, key=lambda x: x[2]):
+        print(p)
+
+
+if __name__ == "__main__":
+    brute()
