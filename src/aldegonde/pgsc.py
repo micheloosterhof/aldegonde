501fb5def3af7d4fab64f9b94728bf777ab7dc08
Michel Oosterhof <michel@oosterhof.net>
2024-03-09 15:15:59 +0800

    black

--- src/aldegonde/pgsc.py
+++ src/aldegonde/pgsc.py
@@ -9,11 +9,9 @@ from typing import Any, Protocol, TypeVar
 
 
 class Comparable(Protocol):
-    def __lt__(self, __other: Any) -> bool:
-        ...
+    def __lt__(self, __other: Any) -> bool: ...
 
-    def __gt__(self, __other: Any) -> bool:
-        ...
+    def __gt__(self, __other: Any) -> bool: ...
 
 
 T = TypeVar("T", bound=Comparable)
