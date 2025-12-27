863c378c22f86e08e0335163491d90bfc3be3c60
Michel Oosterhof <michel@oosterhof.net>
2025-12-27 21:28:28 +0800

    fix import order

--- src/aldegonde/__init__.py
+++ src/aldegonde/__init__.py
@@ -1,6 +1,6 @@
 """Aldegonde library for cryptography and cryptanalysis."""
 
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("aldegonde")
